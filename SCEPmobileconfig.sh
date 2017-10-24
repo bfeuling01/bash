f9dd8753213d0049c35ffcf08a7a2a5ed14ef7e6
Bryan <bfeuling@gmail.com>
2017-10-24 11:48:50 -0500

    Updated with notes and fixes
    Updated the script with notes and better explanations

--- SCEPmobileconfig.sh
+++ SCEPmobileconfig.sh
@@ -9,67 +9,120 @@
 # AUTHOR: Created by Bryan Feuling and Sean Boult
 ###########################################################
 
-currentUser=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
-computerName=`hostname`
-profileName="SCEP Computer TEST"
-profileUUID="C35DB5FE-61A2-4448-8D10-2E6E6EA31846"
-profileID="558B54DB-5FF5-4C8F-A7AB-E64A3693C263"
-endSSID=""
+# Gets current User ID and Computer Name
+CURRUSER=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
+COMPNAME=`hostname`
 
-profileOrg=""
-profileDesc="Used for 802.1X WIFI access"
+# Profile Information
+## Profile mobileconfig requires two different UUIDs
+## for the profile to be setup properly. This is
+## separate from the SCEP profile below.
+PROFNAME="SCEP Computer TEST"
+PROFUUID="C35DB5FE-61A2-4448-8D10-2E6E6EA31846"
+PROFID="558B54DB-5FF5-4C8F-A7AB-E64A3693C263"
+PAYLOADVER=1
 
-#scep stuffz
-SCEPurl=""
-SCEPCAname="CA-SCEP"
-SCEPKeyType="RSA"
-SCEPKeySize="2048"
-SCEPSubject="CN"
-SCEPProfileUUID="GG69CE46-8AAF-4D74-A8BD-5A13933A1547"
-SCEPProfileID="9B95A568-6936-4D48-8C89-4909A5D584B7"
+# Company/Organization Information
+PROFORG=""
+PROFDESC="Used for 802.1X WiFi access"
 
-#wifi stuffz
+# SCEP Information
+## SCEP URL Endpoint
+SCEPURL=""
+
+# SCEP Payload Name
+SCEPDISPNAME="SCEP Profile"
+
+## SCEP Name
+SCEPCANAME="CA-SCEP"
+
+## SCEP Security Type
+SCEPKEYTYPE="RSA"
+SCEPKEYSIZE="2048"
+SCEPSUBJECT="CN"
+
+## SCEP Cert Renewal Time (Number in Days)
+CERTRENEWALTIME=14
+
+# SCEP Profile Identifiers
+## SCEP profile mobile config requires two different UUIDs
+## for the profile to be setup properly.
+SCEPPROFUUID="GG69CE46-8AAF-4D74-A8BD-5A13933A1547"
+SCEPPROFID="9B95A568-6936-4D48-8C89-4909A5D584B7"
+
+# WiFi Information
+# This is to link SCEP with WiFi connection
+## Desired SSID
 WIFISSID=""
-WIFITrustedServerName=""
-WIFIProfileUUID="F04E7E9D-3B46-422E-B36E-DF79787CD9DC8"
-WIFIProfileID="6FC860B4-2166-48C8-8558-A6EFECDC24F7"
-WIFIProfileName=""
-WIFIProxyPac=""
-WIFITTLSInnerAuth="MSCHAPv2"
-WIFIEncryptionType="WPA"
 
+## Desired TLS Trusted Server Name
+TLSTRUSTEDSERVERNAME=""
+
+## WiFi Profile Identifiers
+### WiFi profile mobile config requires two different
+### UUIDs for the profile to be setup properly.
+WIFIPROFUUID="F04E7E9D-3B46-422E-B36E-DF79787CD9DC8"
+WIFIPROFID="6FC860B4-2166-48C8-8558-A6EFECDC24F7"
+
+### Profile Name
+WIFIPROFNAME=""
+
+### WiFi Proxy PAC URL
+WIFIPROXY=""
+
+### WiFi TTLS Inner Authorization Type
+WIFITTLSINNERAUTH="MSCHAPv2"
+
+### Preferred WiFi Encryption Type
+### WPA is WPA2
+WIFIENCRYPTIONTYPE="WPA"
+
+### If a computer is using a provisioning network
+### that is different than the production network,
+### insert the provisioning network in this variable
+### and it will be removed from the computer network
+### list at the end of the script.
+NONDESIREDSSID=""
+
+# FQDN of current computer
 FQDN=""
 
-user=""
-pass=""
+# SCEP service account username and password
+USER=""
+PASS=""
 
-http=`curl -s --ntlm -u $user:$pass <SCEP URL>`
+# cURL request for SCEP
+HTTP=`curl -s --ntlm -u $USER:$PASS $SCEPURL`
 
-MSSCEPCERT=`echo $http | awk -v FS="(<B> | </B>)" '{print $2}'`
-MSSCEPCHALLENGE=`echo $http | awk -v FS="(password is: <B> | </B>)" '{print $3}'`
+# Console Log to ensure that the response is received
+# properly from the SCEP server
+MSSCEPCERT=`echo $HTTP | awk -v FS="(<B> | </B>)" '{print $2}'`
+MSSCEPCHALLENGE=`echo $HTTP | awk -v FS="(password is: <B> | </B>)" '{print $3}'`
 
+## Console Log the above returns
 echo "SCEP: $MSSCEPCHALLENGE"
 echo "SCEPKEY: $MSSCEPCERT"
 
-#this is the xml payload that we insert two vars into and make a mobileconfig out of it
+# SCEP XML mobileconfig payload
+# which has the dynamic variables listed above
 template="<?xml version='1.0' encoding='UTF-8'?>
 <!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
 <plist version='1'>
 	<dict>
 		<key>PayloadUUID</key>
-		<string>$profileUUID</string>
+		<string>$PROFUUID</string>
 		<key>PayloadType</key>
 		<string>Configuration</string>
 		<key>PayloadOrganization</key>
-		<string>$profileOrg</string>
+		<string>$PROFORG</string>
 		<key>PayloadIdentifier</key>
-		<string>$profileID</string>
+		<string>$PROFID</string>
 		<key>PayloadDisplayName</key>
-		<string>$profileName</string>
+		<string>$PROFNAME</string>
 		<key>PayloadDescription</key>
 		<string/>
 		<key>PayloadVersion</key>
-		<integer>1</integer>
+		<integer>$PAYLOADVER</integer>
 		<key>PayloadEnabled</key>
 		<true/>
 		<key>PayloadRemovalDisallowed</key>
@@ -80,63 +133,63 @@ template="<?xml version='1.0' encoding='UTF-8'?>
 		<array>
 			<dict>
 				<key>PayloadUUID</key>
-				<string>$SCEPProfileUUID</string>
+				<string>$SCEPPROFUUID</string>
 				<key>PayloadType</key>
 				<string>com.apple.security.scep</string>
 				<key>PayloadOrganization</key>
-				<string>$profileOrg</string>
+				<string>$PROFORG</string>
 				<key>PayloadIdentifier</key>
-				<string>$SCEPProfileID</string>
+				<string>$SCEPPROFID</string>
 				<key>PayloadDisplayName</key>
-				<string>TEST</string>
+				<string>$SCEPDISPNAME</string>
 				<key>PayloadDescription</key>
 				<string/>
 				<key>PayloadVersion</key>
-				<integer>1</integer>
+				<integer>$PAYLOADVER</integer>
 				<key>PayloadEnabled</key>
 				<true/>
 				<key>PayloadContent</key>
 				<dict>
 					<key>Name</key>
-					<string>$SCEPCAname</string>
+					<string>$SCEPCANAME</string>
 					<key>URL</key>
-					<string>$SCEPurl</string>
+					<string>$SCEPURL</string>
 					<key>Challenge</key>
 					<string>$MSSCEPCHALLENGE</string>
 					<key>Key Type</key>
-					<string>$SCEPKeyType</string>
+					<string>$SCEPKEYTYPE</string>
 					<key>Keysize</key>
-					<integer>$SCEPKeySize</integer>
+					<integer>$SCEPKEYSIZE</integer>
 					<key>SubjectAltName</key>
 					<dict/>
 					<key>Subject</key>
 					<array>
 						<array>
 							<array>
-								<string>$SCEPSubject</string>
+								<string>$SCEPSUBJECT</string>
 								<string>$FQDN</string>
 							</array>
 						</array>
 					</array>
 					<key>CertificateRenewalTimeInterval</key>
-					<integer>14</integer>
+					<integer>$CERTRENEWALTIME</integer>
 				</dict>
 			</dict>
 			<dict>
 				<key>PayloadUUID</key>
-				<string>$WIFIProfileUUID</string>
+				<string>$WIFIPROFUUID</string>
 				<key>PayloadType</key>
 				<string>com.apple.wifi.managed</string>
 				<key>PayloadOrganization</key>
-				<string>$profileOrg</string>
+				<string>$PROFORG</string>
 				<key>PayloadIdentifier</key>
-				<string>$WIFIProfileID</string>
+				<string>$WIFIPROFID</string>
 				<key>PayloadDisplayName</key>
-				<string>$WIFIProfileName</string>
+				<string>$WIFIPROFNAME</string>
 				<key>PayloadDescription</key>
 				<string/>
 				<key>PayloadVersion</key>
-				<integer>1</integer>
+				<integer>$PAYLOADVER</integer>
 				<key>PayloadEnabled</key>
 				<true/>
 				<key>HIDDEN_NETWORK</key>
@@ -144,9 +197,9 @@ template="<?xml version='1.0' encoding='UTF-8'?>
 				<key>SSID_STR</key>
 				<string>$WIFISSID</string>
 				<key>EncryptionType</key>
-				<string>$WIFIEncryptionType</string>
+				<string>$WIFIENCRYPTIONTYPE</string>
 				<key>PayloadCertificateUUID</key>
-				<string>$SCEPProfileUUID</string>
+				<string>$SCEPPROFUUID</string>
 				<key>AutoJoin</key>
 				<true/>
 				<key>AuthenticationMethod</key>
@@ -156,7 +209,7 @@ template="<?xml version='1.0' encoding='UTF-8'?>
 				<key>ProxyType</key>
 				<string>Auto</string>
 				<key>ProxyPACURL</key>
-				<string>$WIFIProxyPac</string>
+				<string>$WIFIPROXY</string>
 				<key>EAPClientConfiguration</key>
 				<dict>
 					<key>AcceptEAPTypes</key>
@@ -164,12 +217,12 @@ template="<?xml version='1.0' encoding='UTF-8'?>
 						<integer>13</integer>
 					</array>
 					<key>TTLSInnerAuthentication</key>
-					<string>$WIFITTLSInnerAuth</string>
+					<string>$WIFITTLSINNERAUTH</string>
 					<key>UserName</key>
 					<string>$FQDN</string>
 					<key>TLSTrustedServerNames</key>
 					<array>
-						<string>$WIFITrustedServerName</string>
+						<string>$TLSTRUSTEDSERVERNAME</string>
 					</array>
 				</dict>
 				<key>SetupModes</key>
@@ -182,14 +235,22 @@ template="<?xml version='1.0' encoding='UTF-8'?>
 	</dict>
 </plist>"
 
-echo $template > /tmp/scep_computer.mobileconfig
+# Create the mobile config from the template above
+echo $TEMPLATE > /tmp/scep_computer.mobileconfig
+
+# Import the above created template
 /usr/bin/profiles -I -F /tmp/scep_computer.mobileconfig
 
-isFound=`profiles -P | grep $profileID`
+# Check if the mobile config is found in the Profile list
+isFound=`profiles -P | grep $PROFID`
 if [[ ! -z $isFound ]]; then 
-	echo "I found the profile in the list!"
+	echo "Profile Successfully Added"
 fi
 
-sudo /usr/sbin/networksetup -removepreferredwirelessnetwork "en0" $endSSID
+# Remove provisioning network from computer network list
+if [ -z $NONDESIREDSSID ]; then
+	sudo /usr/sbin/networksetup -removepreferredwirelessnetwork "en0" $NONDESIREDSSID
+fi
 
+# exiting script
 exit 0
